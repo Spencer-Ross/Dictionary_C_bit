2264debba5704997573772744bc6b04e00a8b3b6
Spencer <spencerthomasr@gmail.com>
2020-02-06 18:56:48 -0800

    Initial commit

---
+++ assignment2/assignment2.c
@@ -0,0 +1,20 @@
+/***********************************************************************
+name: SPENCER ROSS
+	ok -- see if a word is in the online dictionary
+description:
+	Returns 1 if the word exists in the dictionary, otherwise returns 0	
+	See CS 360 IO lecture for details.
+***********************************************************************/
+
+/* Includes and definitions */
+
+
+/**********************************************************************
+Search for given word in dictionary using file descriptor fd.
+Return 1 if word found, else 0.
+**********************************************************************/
+int ok(char *dictionaryName, char *word, int length) {
+	// This is where you do the work ;)
+	return 0;
+}
+
